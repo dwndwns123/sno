64a30e6901454b5a5c0a3c9b967f1a276a5fd7c1
Clive Murray <axemonkey@einaudi.home>
2013-04-08 12:27:01 +0100

    Initial page and bootstrap assets

---
+++ inc/head.php
@@ -0,0 +1,4 @@
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="/css/style.css">
+<script src="/js/html5shiv.js"></script>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php require('inc/head.php'); ?>
+
+  <title>SNOMED Field Test - Home</title>
+</head>
+<body>
+  
+
+  <?php require('inc/foot.php'); ?>
+</body>
+</html>
