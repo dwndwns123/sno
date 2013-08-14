d0c17f0566468d98e4409aa61b91998af8d67b64
Rory Davidson <rory.davidson@gmail.com>
2013-08-14 14:28:26 +0200

    Added Google Analytics for tracking

--- inc/script.php
+++ inc/script.php
@@ -8,7 +8,17 @@
 
 <script src="/js/script.js"></script>
 
+<!-- Google Analytics for site tracking -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
+  ga('create', 'UA-41892858-3', 'snomedtools.com');
+  ga('send', 'pageview');
+
+</script>
 
 
 <div class="well hidden"> 
