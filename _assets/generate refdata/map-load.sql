282e0dc9483f04654facbaad12ca1a13004a2d6b
Rory Davidson <rory.davidson@gmail.com>
2013-08-27 08:10:48 +0200

    Updated map data

--- _assets/generate refdata/map-load.sql
+++ _assets/generate refdata/map-load.sql
@@ -7,7 +7,7 @@ icpc_id varchar(10) not null
 );
 
 load data local 
-	infile '/Users/rory/Dev/SCTUtils/map.txt' 
+	infile '/Users/rory/Dev/SCTUtils/map-20130827.txt' 
 	into table ICPC_SCT_Map
 	columns terminated by '\t' 
 	lines terminated by '\r\n' 
